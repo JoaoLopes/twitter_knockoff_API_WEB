a77cea6f354e9dfdf56325c0f32791fc514a96f3
lopeze <johnny.online@gmail.com>
2016-09-27 22:11:25 +0100

    with users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  resources :users
+
+  scope '/api' do
+    scope '/v1' do
+        resources :users, format: 'json'
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +62,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    username "MyString"
+    hashed_password "MyString"
+    created_by 1
+  end
+end
